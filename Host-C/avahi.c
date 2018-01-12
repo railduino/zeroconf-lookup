0cd098ab6ed4cf3ab8bb25cba0f8da1d88591a62
Volker Wiegand <volker@railduino.de>
2018-01-12 06:40:56 +0100

    updated avahi.c to use C library

--- Host-C/avahi.c
+++ Host-C/avahi.c
@@ -26,125 +26,194 @@
 
 #include "common.h"
 
+#if defined(__linux__)
 
-#define AVAHI_BROWSE_CMD	"/usr/bin/avahi-browse"
-#define AVAHI_BROWSE_ARGS	" --resolve --parsable --no-db-lookup --terminate _http._tcp 2>/dev/null"
+#include <avahi-client/client.h>
+#include <avahi-client/lookup.h>
+#include <avahi-common/simple-watch.h>
+#include <avahi-common/malloc.h>
+#include <avahi-common/error.h>
 
 
-static char answer[1024];
-static result_t *results;
+static AvahiSimplePoll     *my_poll    = NULL;
+static AvahiClient         *my_client  = NULL;
+static AvahiServiceBrowser *my_browser = NULL;
+static result_t            *my_results = NULL;
 
 
 int
-avahi_browse(void)
+avahi_found(void)
 {
-	struct stat sb;
-	FILE *fp;
-	char line[4096], *ptr[32], *tmp;
-	char *rr_name, *rr_target, *rr_a, *rr_port, *rr_txt;
-	int num;
-	result_t *result;
-
-	if (options_use_avahi() != 1) {
-		util_debug("Avahi disabled by configuration");
-		return -1;
-	}
-	if (stat(AVAHI_BROWSE_CMD, &sb) == -1) {
-		util_debug("Avahi not found where expected");
-		return -1;
-	}
-	if ((fp = popen(AVAHI_BROWSE_CMD AVAHI_BROWSE_ARGS, "r")) == NULL) {
-		util_error("Avahi found, but popen gave %s", strerror(errno));
-		return -1;
-	}
-	util_info("using avahi-browse for discovery");
-
-	while (fgets(line, sizeof(line), fp) != NULL) {
-		for (num = 0; num < 32; num++) {
-			ptr[num] = strtok(num ? NULL : line, ";\n");
-			if (ptr[num] == NULL) {
-				break;
-			}
-		}
-		if (num < 9 || strcmp(ptr[0], "=") != 0) {
-			continue;
-		}
-		if (strcmp(ptr[2], "IPv4") != 0) {
-			continue;
-		}
-		rr_name   = ptr[3];
-		rr_target = ptr[6];
-		rr_a      = ptr[7];
-		rr_port   = ptr[8];
-		rr_txt    = (num >= 10) ? ptr[9] : "";
-
-		for (tmp = ptr[3]; *tmp != '\0'; tmp++) {
-			if (*tmp == '\\' && tmp[1] >= '0' && tmp[1] <= '9' \
-					 && tmp[2] >= '0' && tmp[2] <= '9' \
-					 && tmp[3] >= '0' && tmp[3] <= '9') {
-				num = (int) (tmp[1] - '0') * 100 + (int) (tmp[2] - '0') * 10  + (int) (tmp[3] - '0');
-				tmp[0] = (char) num;
-				memmove(tmp + 1, tmp + 4, strlen(tmp + 3));
-			}
-		}
-
-		if (strcmp(rr_port, "3689") == 0) {
-			rr_txt = "DAAP (iTunes) Server";
-		} else if (rr_txt[0] == '"') {
-			rr_txt++;
-			if ((num = strlen(rr_txt)) > 0 && rr_txt[num - 1] == '"') {
-				rr_txt[num - 1] = '\0';
-			}
-		}
-
-		UTIL_STRCPY(answer, "    {\n");
-		util_append(answer, sizeof(answer), "      \"name\": \"%s\",\n",   rr_name);
-		util_append(answer, sizeof(answer), "      \"txt\": \"%s\",\n",    rr_txt);
-		util_append(answer, sizeof(answer), "      \"target\": \"%s\",\n", rr_target);
-		util_append(answer, sizeof(answer), "      \"port\": %s,\n",       rr_port);
-		util_append(answer, sizeof(answer), "      \"weight\": %u,\n",     0);
-		util_append(answer, sizeof(answer), "      \"priority\": %u,\n",   0);
-		util_append(answer, sizeof(answer), "      \"a\": \"%s\",\n",      rr_a);
-		util_append(answer, sizeof(answer), "      \"url\": \"http://%s:%s/\"\n", rr_a, rr_port);
-		UTIL_STRCAT(answer, "    }");
-
-		result = util_malloc(sizeof(result_t));
-		result->next = results;
-		result->text = util_strdup(answer);
-		results = result;
-	}
-
-	pclose(fp);
-	return 0;
-}
-
-
-result_t *
-avahi_get_result(void)
-{
-	return results;
+	return 1;
 }
 
 
 static void
 avahi_cleanup(void)
 {
-	result_t *result;
+	result_t *tmp;
 
-	while (results != NULL) {
-		result = results->next;
-		util_free(results->text);
-		util_free(results);
-		results = result;
+	if (my_browser != NULL) {
+		avahi_service_browser_free(my_browser);
+		my_browser = NULL;
+	}
+
+	if (my_client != NULL) {
+		avahi_client_free(my_client);
+		my_client = NULL;
+	}
+
+	if (my_poll != NULL) {
+		avahi_simple_poll_free(my_poll);
+		my_poll = NULL;
+	}
+
+	while (my_results != NULL) {
+		tmp = my_results->next;
+		util_free(my_results->text);
+		util_free(my_results);
+		my_results = tmp;
 	}
 }
 
 
-void
-avahi_init(void)
+static void
+avahi_resolve_callback(AvahiServiceResolver *r, AVAHI_GCC_UNUSED AvahiIfIndex interface,
+		AVAHI_GCC_UNUSED AvahiProtocol protocol, AvahiResolverEvent event,
+		const char *name, AVAHI_GCC_UNUSED const char *type,
+		AVAHI_GCC_UNUSED const char *domain, const char *host_name,
+		const AvahiAddress *address, uint16_t port, AvahiStringList *txt,
+		AVAHI_GCC_UNUSED AvahiLookupResultFlags flags, AVAHI_GCC_UNUSED void *userdata)
 {
-	results = NULL;
+	char answer[FILENAME_MAX], tmp_adr[AVAHI_ADDRESS_STR_MAX], tmp_txt[FILENAME_MAX];
+	result_t *result;
+
+	if (event == AVAHI_RESOLVER_FAILURE) {
+		util_error("avahi_resolve_callback() error %s", avahi_strerror(avahi_client_errno(avahi_service_resolver_get_client(r))));
+		avahi_service_resolver_free(r);
+		return;
+	}
+	if (event != AVAHI_RESOLVER_FOUND) {
+		util_debug(2, "avahi_resolve_callback() unknown event %d", event);
+		avahi_service_resolver_free(r);
+		return;
+	}
+
+	avahi_address_snprint(tmp_adr, sizeof(tmp_adr), address);
+	if (port == 3689) {
+		UTIL_STRCPY(tmp_txt, "DAAP (iTunes) Server");
+	} else {
+		char *t = avahi_string_list_to_string(txt);
+		UTIL_STRCPY(tmp_txt, t);
+		avahi_free(t);
+	}
+
+	UTIL_STRCPY(answer, "    {\n");
+	util_append(answer, sizeof(answer), "      \"name\": \"%s\",\n",   name);
+	util_append(answer, sizeof(answer), "      \"txt\": \"%s\",\n",    util_strtrim(tmp_txt, '"'));
+	util_append(answer, sizeof(answer), "      \"target\": \"%s\",\n", host_name);
+	util_append(answer, sizeof(answer), "      \"port\": %u,\n",       port);
+	util_append(answer, sizeof(answer), "      \"a\": \"%s\",\n",      tmp_adr);
+	util_append(answer, sizeof(answer), "      \"url\": \"http://%s:%u/\"\n", tmp_adr, port);
+	UTIL_STRCAT(answer, "    }");
+
+	avahi_service_resolver_free(r);
+
+	for (result = my_results; result != NULL; result = result->next) {
+		if (strcmp(result->text, answer) == 0) {
+			return;		// duplicate entry
+		}
+	}
+
+	result = util_malloc(sizeof(result_t));
+	result->next = my_results;
+	result->text = util_strdup(answer);
+	my_results = result;
+}
+
+
+static void
+avahi_browse_callback(AvahiServiceBrowser *b, AvahiIfIndex interface, AvahiProtocol protocol,
+		AvahiBrowserEvent event, const char *name, const char *type, const char *domain,
+		AVAHI_GCC_UNUSED AvahiLookupResultFlags flags, void *userdata)
+{
+	AvahiClient *c = userdata;
+
+	if (event == AVAHI_BROWSER_FAILURE) {
+		util_error("avahi_browse_callback() error %s", avahi_strerror(avahi_client_errno(avahi_service_browser_get_client(b))));
+		avahi_simple_poll_quit(my_poll);
+		return;
+	}
+
+	if (event == AVAHI_BROWSER_NEW) {
+		if (avahi_service_resolver_new(c, interface, protocol, name, type, domain,
+				AVAHI_PROTO_UNSPEC, 0, avahi_resolve_callback, c) == NULL) {
+			util_error("avahi_browse_callback() error for %s: %s", name, avahi_strerror(avahi_client_errno(c)));
+		}
+		return;
+	}
+
+	 if (event == AVAHI_BROWSER_ALL_FOR_NOW) {
+		avahi_simple_poll_quit(my_poll);
+	}
+}
+
+
+static void
+avahi_client_callback(AvahiClient *c, AvahiClientState state, AVAHI_GCC_UNUSED void *userdata)
+{
+	if (state == AVAHI_CLIENT_FAILURE) {
+		util_error("avahi_client_callback() error %s", avahi_strerror(avahi_client_errno(c)));
+		avahi_simple_poll_quit(my_poll);
+	}
+}
+
+
+result_t *
+avahi_browse(void)
+{
+	int error;
 
 	atexit(avahi_cleanup);
+
+	my_poll = avahi_simple_poll_new();
+	if (my_poll == NULL) {
+		util_error("avahi_simple_poll_new() failed");
+		return NULL;
+	}
+
+	my_client = avahi_client_new(avahi_simple_poll_get(my_poll), 0, avahi_client_callback, NULL, &error);
+	if (my_client == NULL) {
+		util_error("avahi_client_new() error %s", avahi_strerror(error));
+		return NULL;
+	}
+
+	my_browser = avahi_service_browser_new(my_client, AVAHI_IF_UNSPEC, AVAHI_PROTO_UNSPEC,
+			"_http._tcp", NULL, 0, avahi_browse_callback, my_client);
+	if (my_browser == NULL) {
+		fprintf(stderr, "avahi_service_browser_new() error %s", avahi_strerror(avahi_client_errno(my_client)));
+		return NULL;
+	}
+
+	avahi_simple_poll_loop(my_poll);
+
+	return my_results;
 }
+
+#else /* __linux__ */
+
+int
+avahi_found(void)
+{
+	return 0;
+}
+
+
+result_t *
+avahi_browse(void)
+{
+	return NULL;
+}
+
+#endif /* __linux__ */
 
