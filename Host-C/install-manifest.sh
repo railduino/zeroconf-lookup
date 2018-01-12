84b76a82b3d24257ead127668a9f41fdad6b772f
Volker Wiegand <volker@railduino.de>
2018-01-12 07:55:23 +0100

    fixed paths in and for JSON manifests

--- Host-C/install-manifest.sh
+++ Host-C/install-manifest.sh
@@ -27,13 +27,29 @@ set -e
 
 _extens="knldjmfmopnpolahpmmgbagdohdnhkik"
 _jsonid="com.railduino.zeroconf_lookup"
-_inform="Find HTTP Services in the .local domain"
+_inform="Find HTTP Services in the .local domain using Zeroconf (mDNS-SD)"
 _binary="zeroconf_lookup"
 
-if [[ $1 == "user" ]] ; then
+if [[ $1 == "local" ]] ; then
 	mkdir -p $HOME/bin
 	rm -f $HOME/bin/$_binary
 	install -v -m 0755 $_binary $HOME/bin
+
+	_dest=$HOME/.mozilla/native-messaging-hosts
+	mkdir -p $_dest
+	_file=$_dest/$_jsonid.json
+
+	cat >$_file <<-_EOF_
+		{
+		  "name": "$_jsonid",
+		  "description": "$_inform",
+		  "path": "$HOME/bin/$_binary",
+		  "type": "stdio",
+		  "allowed_extensions": [ "zeroconf_lookup@railduino.com" ]
+		}
+	_EOF_
+	chmod 0644 $_file
+	echo "created $_file"
 
 	for _dest in	$HOME/.config/google-chrome/NativeMessagingHosts \
 			$HOME/.config/chromium/NativeMessagingHosts ; do
@@ -53,7 +69,14 @@ if [[ $1 == "user" ]] ; then
 		echo "created $_file"
 	done
 
-	_dest=$HOME/.mozilla/native-messaging-hosts
+	exit 0
+fi
+
+if [[ $1 == "system" ]] ; then
+	rm -f /usr/bin/$_binary
+	install -v -o root -g root -m 0755 $_binary /usr/bin
+
+	_dest=/usr/lib/mozilla/native-messaging-hosts
 	mkdir -p $_dest
 	_file=$_dest/$_jsonid.json
 
@@ -61,20 +84,13 @@ if [[ $1 == "user" ]] ; then
 		{
 		  "name": "$_jsonid",
 		  "description": "$_inform",
-		  "path": "$HOME/bin/$_binary",
+		  "path": "/usr/bin/$_binary",
 		  "type": "stdio",
-		  "allowed_extensions": [ "$_binary@railduino.com" ]
+		  "allowed_extensions": [ "zeroconf_lookup@railduino.com" ]
 		}
 	_EOF_
 	chmod 0644 $_file
 	echo "created $_file"
-
-	exit 0
-fi
-
-if [[ $1 == "root" ]] ; then
-	rm -f /usr/bin/$_binary
-	install -v -o root -g root -m 0755 $_binary /usr/bin
 
 	for _dest in	/etc/opt/chrome/native-messaging-hosts \
 			/etc/chromium/native-messaging-hosts ; do
@@ -93,22 +109,6 @@ if [[ $1 == "root" ]] ; then
 		chmod 0644 $_file
 		echo "created $_file"
 	done
-
-	_dest=/usr/lib/mozilla/native-messaging-hosts
-	mkdir -p $_dest
-	_file=$_dest/$_jsonid.json
-
-	cat >$_file <<-_EOF_
-		{
-		  "name": "$_jsonid",
-		  "description": "$_inform",
-		  "path": "/usr/bin/$_binary",
-		  "type": "stdio",
-		  "allowed_extensions": [ "zeroconf_lookup@railduino.com" ]
-		}
-	_EOF_
-	chmod 0644 $_file
-	echo "created $_file"
 
 	exit 0
 fi
